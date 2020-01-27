f40ebc3f9a9ece589b1e0ad37b0a6b3c4cfebaf0
pratiti <pratitipshah@gmail.com>
2020-01-27 18:42:19 +0530

    ADDED : step 4-5 - return invoice ADDED : premium and normal categories

--- CabInvoiceGenerator.js
+++ CabInvoiceGenerator.js
@@ -1,9 +1,14 @@
 
-const BASE_FAIR = 5;
-const FARE_PER_KM = 10;
-const FARE_PER_MIN = 1;
+const BASE_FAIR_NORMAL = 5;
+const FARE_PER_KM_NORMAL = 10;
+const FARE_PER_MIN_NORMAL = 1;
+const BASE_FAIR_PREMIUM = 20;
+const FARE_PER_KM_PREMIUM = 15;
+const FARE_PER_MIN_PREMIUM = 2;
+const CATEGORY_NORMAL = 'normal';
+const CATEGORY_PREMIUM = 'premium';
 
-function calculateFare(distance, time) {
+function calculateFare(distance, time, category) {
 
     //case 1 - check if no arguments
     if (distance == "" || time == "")
@@ -20,9 +25,20 @@ function calculateFare(distance, time) {
     if (distance < 0 || time < 0)
         return 0;
 
-    //case 5 - calculate fare for passed values
 
-    let fare = BASE_FAIR + distance * FARE_PER_KM + time * FARE_PER_MIN;
+    //for category
+    //case 1 - check if category is undefined
+    if (typeof (category) == 'undefined')
+        category = normal;
+
+    //calculate fare for passed values
+    let fare = 0;
+    if (category == CATEGORY_NORMAL) {
+        fare = BASE_FAIR_NORMAL + distance * FARE_PER_KM_NORMAL + time * FARE_PER_MIN_NORMAL;
+    } else if (category == CATEGORY_PREMIUM) {
+        fare = BASE_FAIR_PREMIUM + distance * FARE_PER_KM_PREMIUM + time * FARE_PER_MIN_PREMIUM;
+
+    }
     return fare;
 }
 
@@ -34,24 +50,27 @@ function calAggregateFare(rides) {
 
     let totalAggregateFare = 0;
     for (ride of rides) {
-        totalAggregateFare = totalAggregateFare + calculateFare(ride.distance, ride.time);
+        totalAggregateFare = totalAggregateFare + calculateFare(ride.distance, ride.time, ride.cat);
     }
     console.log(totalAggregateFare);
     return totalAggregateFare;
 }
 
-function cabInvoiceGenerator(rides){
-    let totalAggregateFare=calAggregateFare(rides);
-    let nRides=rides.length;
-    let averageFarePerRide=totalAggregateFare/nRides;
-    let invoice={
-        totalFare:totalAggregateFare,
-        noOfRides:rides.length,
-        averageFare:Number((totalAggregateFare/nRides).toFixed(2))
-        }
+function cabInvoiceGenerator(rides) {
+    let totalAggregateFare = calAggregateFare(rides);
+    let nRides = rides.length;
+    let averageFarePerRide = Number((totalAggregateFare / nRides).toFixed(2));
+    let invoice = {
+        totalFare: totalAggregateFare,
+        noOfRides: rides.length,
+        averageFare: averageFarePerRide
+    }
     return invoice;
 
 }
 
-let rides = [{ distance: 5, time: 8 }, { distance: 15, time: 20 }, { distance: 30, time: 40 }];
+let rides = [
+    { distance: 5, time: 8, cat:'normal'}, 
+    { distance: 15, time: 20, cat:'premium' }, 
+    { distance: 30, time: 40, cat:'normal' }];
 cabInvoiceGenerator(rides);
