6953ae874f0f32de5315b5369c4f9c0a80a594d7
pratiti <pratitipshah@gmail.com>
2020-01-28 11:30:28 +0530

    REFACTORED : code refactored for TC4 - passed

--- UnitConversionController.js
+++ UnitConversionController.js
@@ -8,10 +8,10 @@ exports.feetToInchConversion=function(feet){
     //case 2 - type check
     if(isNaN(feet))
         return 0;
-    //case 3 - negative arguments
+    //case 3 - check negative arguments
     if(feet<0)
         return 0;
-        
+
     let inch=feet*12;
     return inch;
 }
