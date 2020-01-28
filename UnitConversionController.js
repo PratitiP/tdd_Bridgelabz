38b926bb3b7f3c50d27a821438b17820d36f07e7
pratiti <pratitipshah@gmail.com>
2020-01-28 11:26:37 +0530

    ADDED : code refactored for TC3 - passed

--- UnitConversionController.js
+++ UnitConversionController.js
@@ -1,10 +1,14 @@
 
 
 exports.feetToInchConversion=function(feet){
-    
+    //case 1 - null check
     if(feet==undefined)
         return 0;
-         
+
+    //case 2 - type check
+    if(isNaN(feet))
+        return 0;
+    
     let inch=feet*12;
     return inch;
 }
