ea1d42cf18cd4fe0529e83f9dc53331ba1ed15dd
pratiti <pratitipshah@gmail.com>
2020-01-28 11:38:26 +0530

    ADDED : all TC for inch - passed

--- UnitConversion.test.js
+++ UnitConversion.test.js
@@ -26,6 +26,31 @@ describe('Tests for function feetToInch with feet number',function(){
         let res=unitConvertor.feetToInchConversion(1);
         assert.equal(res,12);
     });
+});
 
+describe('Tests for function inchToFeet with inch number',function(){
+    it('check function for null argument',function(){
+        let res=unitConvertor.inchToFeetConversion();
+        assert.equal(res,0);
+    });
 
+    it('check function for 0 feet argument',function(){
+        let res=unitConvertor.inchToFeetConversion(0);
+        assert.equal(res,0);
+    });
+
+    it('check function for NaN argument - type check',function(){
+        let res=unitConvertor.inchToFeetConversion('s');
+        assert.equal(res,0);
+    });
+
+    it('check function for negative argument',function(){
+        let res=unitConvertor.inchToFeetConversion(-5);
+        assert.equal(res,0);
+    });
+
+    it('check function - value check for quality',function(){
+        let res=unitConvertor.inchToFeetConversion(12);
+        assert.equal(res,1);
+    });
 });
--- UnitConversionController.js
+++ UnitConversionController.js
@@ -15,3 +15,19 @@ exports.feetToInchConversion=function(feet){
     let inch=feet*12;
     return inch;
 }
+
+exports.inchToFeetConversion=function(inch){
+    //case 1 - null check
+    if(inch==undefined)
+        return 0;
+
+    //case 2 - type check
+    if(isNaN(inch))
+        return 0;
+    //case 3 - check negative arguments
+    if(inch<0)
+        return 0;
+
+    let feet=inch/12;
+    return feet;
+}
